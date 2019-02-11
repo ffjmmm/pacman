ef38823554284b8a88c372fb54015c124fd7296d
ffjmmm <ffjmmm@outlook.com>
2019-02-11 10:36:24 -0800

    update

--- myAgents.py
+++ myAgents.py
@@ -131,7 +131,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
             is the incremental cost of expanding to that successor
         """
         successors = []
-        for action in [Directions.NORTH, Directions.EAST, Directions.WEST, Directions.SOUTH]:
+        for action in [Directions.NORTH, Directions.EAST, Directions.SOUTH, Directions.WEST]:
             x, y = state
             dx, dy = Actions.directionToVector(action)
             nextx, nexty = int(x + dx), int(y + dy)
