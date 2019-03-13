1cb93050a117b062d0732a4040fbbbe844abbfb1
ffjmmm <ffjmmm@outlook.com>
2019-03-12 22:22:51 -0700

    update

---
+++ build/src/CMakeFiles/pathtracer.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/application.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/application.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/bbox.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/bbox.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/bsdf.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/bsdf.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/bvh.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/bvh.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/camera.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/camera.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/collada/camera_info.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/collada/camera_info.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/collada/collada.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/collada/collada.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/collada/light_info.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/collada/light_info.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/collada/material_info.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/collada/material_info.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/collada/polymesh_info.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/collada/polymesh_info.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/collada/sphere_info.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/collada/sphere_info.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/dynamic_scene/mesh.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/dynamic_scene/mesh.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/dynamic_scene/scene.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/dynamic_scene/scene.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/dynamic_scene/sphere.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/dynamic_scene/sphere.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/halfEdgeMesh.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/halfEdgeMesh.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/main.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/main.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/meshEdit.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/meshEdit.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/misc/sphere_drawing.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/misc/sphere_drawing.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/pathtracer.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/pathtracer.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/sampler.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/sampler.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/static_scene/environment_light.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/static_scene/environment_light.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/static_scene/light.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/static_scene/light.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/static_scene/object.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/static_scene/object.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/static_scene/sphere.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/static_scene/sphere.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/src/static_scene/triangle.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/src/CMakeFiles/pathtracer.dir/static_scene/triangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "/usr/include/freetype2"
+  "/usr/include/x86_64-linux-gnu/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
