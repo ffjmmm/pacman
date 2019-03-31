288bbeab37c97944b88267869255e75ccd9030cd
ffjmmm <ffjmmm@outlook.com>
2019-03-31 16:15:15 -0700

    update

---
+++ build/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/base64.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/base64.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/color.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/color.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/complex.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/complex.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/lodepng.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/lodepng.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/matrix3x3.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/matrix3x3.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/matrix4x4.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/matrix4x4.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/osdfont.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/osdfont.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/osdtext.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/osdtext.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/path.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/path.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/quaternion.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/quaternion.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/spectrum.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/spectrum.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/tinyxml2.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/tinyxml2.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/vector2D.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/vector2D.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/vector3D.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/vector3D.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/vector4D.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/vector4D.cpp.o"
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/CGL/src/viewer.cpp" "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/src/CMakeFiles/CGL.dir/viewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "../CGL/deps/glew/include"
+  "/usr/include/freetype2"
+  "/usr/include/x86_64-linux-gnu/freetype2"
+  "../CGL/include/CGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cc/cs184/sp19/class/cs184-ahw/pacman/build/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
